c5cadb850ec759b18c3348b36e85bb16d0308d40
문기현 <john7266@naver.com>
2023-11-09 13:51:08 +0900

    20231109 Commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,44 @@
-import { useEffect } from 'react';
+/* import { useEffect } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
 
 export async function getServerSideProps() {
   // 이 곳에서 Rest API 호출
-  //const { data } = await axios.get(`${process.env.API_ENDPOINT}/api/04/users`);
-  const usersReq = await axios.get('https://api.rwnjs.com/04/users')
+  const { data } = await axios.get(`${process.env.API_ENDPOINT}/api/04/users`);
+  //const usersReq = await axios.get('https://api.rwnjs.com/04/users')
   return {
     props: {
-      users: usersReq.data,
+      users: data,
     },
   };
   
+}
+
+export default function HomePage({ users }) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link href={`/users/${user.username}`} passHref>
+            {user.username}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}*/
+
+import Link from 'next/link';
+import axios from 'axios';
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(`${process.env.API_ENDPOINT}/api/04/users`);
+
+  return {
+    props: {
+      users: data,
+    },
+  };
 }
 
 export default function HomePage({ users }) {
